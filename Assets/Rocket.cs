3d64b5aa36ad202434e68dc90760a49eb52cb413
Jakub Kulesza <jakub.kulesza.ofc@gmail.com>
2019-07-17 15:20:09 +0200

    1st level implementation

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float shipThrust = 10f;
+
     AudioSource audioSource;
     Rigidbody rigidBody;
 
@@ -19,29 +22,54 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                print("OK"); //todo remove print
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("DEAD");
+                break;
+        }
+    }
+    void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             audioSource.UnPause();
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * shipThrust);
         }
         else
         {
             audioSource.Pause();
         }
+    }
+    void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
-            print("Rotating left");
+            transform.Rotate(Vector3.forward * rotationSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward * rotationSpeed);
         }
+        rigidBody.freezeRotation = false;
     }
+
 }
